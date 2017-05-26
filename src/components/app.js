f1519c0af7dc7731ddc2c6ea465c41f63d466ffc
Alex Windle <AlexWind01@C02TF1FZG8WL.local>
2017-05-26 14:34:06 -0400

    Fetch and test data from Rijksmuseum API.

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
+import $ from 'jquery'; 
+
+// (1) fetch data
+fetch("https://www.rijksmuseum.nl/api/en/collection/sk-c-5?key=cbUNdwH5")
+	.then(function(response) {
+		return response.json(); 
+	}).then(function(obj) { 
+		console.log("Title: ", obj.artObject.title);
+		console.log("Artist: ", obj.artObject.principalOrFirstMaker);
+		console.log("Date: ", obj.artObject.dating.presentingDate); 
+		console.log("Medium: ", obj.artObject.physicalMedium);
+		console.log("Image: ", obj.artObject.webImage.url);
+	}); 
+
+// (2) ajax call 
+// $.ajax({
+// 	url: "https://www.rijksmuseum.nl/api/en/collection/sk-c-5?key=cbUNdwH5&format=json",
+// 	type: 'GET' 
+// }).then(function(obj) {
+// 	console.log("Title: ", obj.artObject.title);
+// 	console.log("Artist: ", obj.artObject.principalOrFirstMaker);
+// 	console.log("Date: ", obj.artObject.dating.presentingDate); 
+// 	console.log("Medium: ", obj.artObject.physicalMedium);
+// 	console.log("Image: ", obj.artObject.webImage.url);  
+// 	}); 
 
 export default class App extends Component {
+
   render() {
     return (
-      <div>Museum App</div>
+      <div>
+      	Museum App
+      </div>
     );
   }
 }
