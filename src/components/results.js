944069e0050919b5b54a9633c7fb7eb2399cfa8e
Alex Windle <AlexWind01@C02TF1FZG8WL.local>
2017-05-31 16:25:43 -0400

    Map title, artist and thumbnail to page.

--- src/components/results.js
+++ src/components/results.js
@@ -2,9 +2,29 @@ import React from 'react';
 
 const Results = function(props) {
 	console.log(props.artworks);
-	
+
 	const artworkList = props.artworks.map(function(artwork) {
-		return (<li>{artwork.title}</li>);
+		if(artwork.webImage) {
+			return (
+				<li>
+					<strong>Title:</strong><em> {artwork.title}</em><br/>
+					<strong>Artist:</strong> {artwork.principalOrFirstMaker}<br/><br/>
+					<div className="img-wrapper">
+						<img src={artwork.webImage.url} alt="No image available." />
+					</div>
+				</li>
+			);
+		} else {
+			return (
+				<li>
+					<strong>Title:</strong><em> {artwork.title}</em><br/>
+					<strong>Artist:</strong> {artwork.principalOrFirstMaker}<br/><br/>
+					<div className="img-wrapper">
+						<img src={null} alt="No image available." />
+					</div>
+				</li>
+			); 
+		}
 	})
 
 	return (
