ac3ccb73a5f6e7c5e11ee32a296e92328fde7467
Alex Windle <AlexWind01@C02TF1FZG8WL.local>
2017-05-31 09:43:24 -0400

    Add state to search bar. Bind change handler.

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'; 
 
 export default class SearchBar extends Component {
+	constructor() {
+		super(); 
+		this.state = {searchTerm: ''};
+	} 
+
+	handleChange(event) {
+		this.setState({searchTerm: event.target.value}); 
+		console.log(this.state.searchTerm);  
+	}
+
 	render(){
 		return (
 			<div>
-				<input type="text"/>
+				<input 
+					type="text"
+					value={this.state.searchTerm}
+					onChange={this.handleChange.bind(this)}
+				/>
 			</div>
 		);
 	}
