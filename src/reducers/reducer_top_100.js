ecd6001b4ee43ba1ea17897f4dd0e7faae1bd7e6
Alex Windle <AlexWind01@C02TF1FZG8WL.local>
2017-06-07 12:47:44 -0400

    ...fix data model.

--- src/reducers/reducer_top_100.js
+++ src/reducers/reducer_top_100.js
@@ -1,18 +1,26 @@
-function() {
-	// fetch data from API
-	const url = "https://www.rijksmuseum.nl/api/en/collection/" +
-		"?key=cbUNdwH5" + 
-		"&ps=20" +
-		"&imgonly=True" + 
-		"&toppieces=True"; 
+import $ from 'jquery'; 
 
-		$.ajax({
-			url: url,
-			type: 'GET'
-		}).then(function(obj) {
-			return obj.artObjects;
-		}).then(array => console.table(array));
-	}
+export default function() {
+	// fetch data from API
+	// const url = "https://www.rijksmuseum.nl/api/en/collection/" +
+	// 	"?key=cbUNdwH5" + 
+	// 	"&ps=20" +
+	// 	"&imgonly=True" + 
+	// 	"&toppieces=True"; 
+
+	// 	$.ajax({
+	// 		url: url,
+	// 		type: 'GET'
+	// 	}).then(function(obj) {
+	// 		return obj.artObjects;
+	// 	}).then(array => {
+	// 		console.log("Ajax runs...");
+	// 		return array;
+	// 	});
 
 	// return a data object to populate state
+	return [
+		{thing: "thing"}, 
+		{thing: "thing2"}
+	]
 }
