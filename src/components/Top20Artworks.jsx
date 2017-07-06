54d18e85d877131d2bab8ae1ca1827298c01e8d9
Alex Windle <Alex-Windle@users.noreply.github.com>
2017-07-06 16:09:40 -0400

    Convert Top20Artworks from functional to class component. onClick function problem: not grabbing correct list item.

--- src/components/Top20Artworks.jsx
+++ src/components/Top20Artworks.jsx
@@ -5,29 +5,42 @@ import $ from 'jquery';
 // Component imports
 import DetailView from '../components/DetailView'; 
 
-export default (props) => {
-	const { topArtworks } = props; 
+class Top20Artworks extends Component {
+	constructor(props) {
+		super(props);
 
-	const renderList = topArtworks.map(artwork => {
+		this.handleClick = this.handleClick.bind(this);
+	}; 
+
+	handleClick(e) {
+		console.log(e);
+	};
+
+	render() {
+		const { topArtworks } = this.props; 
+		const renderList = topArtworks.map(artwork => {
+			return (
+				<li key={artwork.id} onClick={this.handleClick}>
+					<img src={artwork.webImage.url} height="70px" /> 
+					{artwork.title} 
+				</li>
+			);
+		});
 		return (
-			<li key={artwork.id}>
-				<img src={artwork.webImage.url} height="70px" /> 
-				{artwork.title} 
-			</li>
-		);
-	});
-
-	return (
-		<div>
-			<strong>Explore</strong> top artworks housed in the museum.
-			<div className="top20-detail-wrapper">
-				<ul className="top20-list">
-					{renderList}
-				</ul>
-				<div className="detail-view">
-					<DetailView />
+			<div>
+				<strong>Explore</strong> top artworks housed in the museum.
+				<div className="top20-detail-wrapper">
+					<ul className="top20-list">
+						{renderList}
+					</ul>
+					<div className="detail-view">
+						<DetailView />
+					</div>
 				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
+
+export default Top20Artworks;
+
