5525b16efbad9c6aeddabe6eb87adfe70e9de485
Alex Windle <Alex-Windle@users.noreply.github.com>
2017-07-06 11:28:53 -0400

    Adds middleware to Redux store.

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './store/createStore'; 
+import createStoreWithMiddleware from './store/createStore'; 
 
 // Component imports
 import App from './components/app.jsx';
 
 // Reducer imports 
 import { rootReducer } from './reducers/index'; 
+import reducers from './reducers/index';
 
 ReactDOM.render(
-    <Provider store={store}>
+    <Provider store={createStoreWithMiddleware(
+    	reducers,
+    	{}, 
+    	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    )}>
     	<App />
     </Provider>
     ,document.querySelector('.container')
--- src/store/createStore.js
+++ src/store/createStore.js
@@ -1,11 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
-import reducers from '../reducers/index';
+// import reducers from '../reducers/index';
 import ReduxPromise from 'redux-promise';
 
-const store = createStore(
-  reducers, 
-  {}, 
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
+// const store = createStore(
+//   reducers, 
+//   {}, 
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+// );
 
-export default store;
+// Hook middlware into the application
+const createStoreWithMiddleWare = applyMiddleware(ReduxPromise)(createStore); 
+
+export default createStoreWithMiddleWare;
