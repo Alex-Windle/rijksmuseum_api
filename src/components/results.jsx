270e42534948a897bb600ae623d4c2ffc59e88ef
Alex Windle <Alex-Windle@users.noreply.github.com>
2017-07-12 13:03:40 -0400

    Results rendering to UI.

--- src/components/results.jsx
+++ src/components/results.jsx
@@ -1,38 +1,39 @@
 import React from 'react'; 
 
 const Results = function(props) {
-	// const artworkList = props.artworks.map(function(artwork) {
-	// 	if (artwork.webImage) {
-	// 		return (
-	// 			<li key={artwork.id}>
-	// 				<div className="img-wrapper"><img src={artwork.webImage.url} alt="No image available." /></div>
-	// 				<em>{artwork.title}</em><br/>
-	// 				{artwork.principalOrFirstMaker}
-	// 			</li>
-	// 		);
-	// 	} else {
-	// 		return (
-	// 			<li key={artwork.id}>
-	// 				<div className="img-wrapper-empty">No image available.</div>
-	// 				<em>{artwork.title}</em><br/>
-	// 				{artwork.principalOrFirstMaker}
-	// 			</li>
-	// 		);
-	// 	}
-	// })
+	const results = props.results.map(function(result) {
+		if (result.webImage) {
+			return (
+				<li key={result.id}>
+					<div className="img-wrapper"><img src={result.webImage.url} alt="No image available." /></div>
+					<em>{result.title}</em><br/>
+					{result.principalOrFirstMaker}
+				</li>
+			);
+		} else {
+			return (
+				<li key={result.id}>
+					<div className="img-wrapper-empty">No image available.</div>
+					<em>{result.title}</em><br/>
+					{result.principalOrFirstMaker}
+				</li>
+			);
+		}
+	})
 
-	// return (
-	// 	<div>
-	// 		<strong>Results</strong>
-	// 		<ul className="artwork-list">{artworkList}</ul>
-	// 	</div>
-	// );
-	console.log('Results: ', props.results);
 	return (
 		<div>
-			Results
+			<strong>Results</strong>
+			<ul className="artwork-list">{results}</ul>
 		</div>
 	);
+
+	// console.log('Results: ', props.results);
+	// return (
+	// 	<div>
+	// 		Results
+	// 	</div>
+	// );
 }
 
 export default Results;
